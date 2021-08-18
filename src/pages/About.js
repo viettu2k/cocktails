27e2dfadc2eab4afd87cf1186ce8e420001d54cf
viettu2k <doanviettu2k@gmail.com>
2021-08-18 17:28:07 +0700

    fifteenth project react tutorial

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <section className="section about-section">
+            <h1 className="section-title">about us</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Distinctio, labore exercitationem nam rem ab tenetur in fugit
+                quaerat dolore dolores temporibus sit praesentium porro qui
+                inventore quisquam magnam cum? Molestiae.
+            </p>
+        </section>
+    );
+};
+
+export default About;
